1f621d08ef6c26c6663db9b9d47a2975308a3e76
laithabdel <32340324+laithabdel@users.noreply.github.com>
2022-02-13 15:58:40 -0500

    changing themes and initial start

--- app/src/main/java/com/android/dunderbaks/SplashActivity.kt
+++ app/src/main/java/com/android/dunderbaks/SplashActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
-import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
--- app/src/main/java/com/android/dunderbaks/core/dashboard/TapListFragment.kt
+++ app/src/main/java/com/android/dunderbaks/core/dashboard/TapListFragment.kt
@@ -2,7 +2,6 @@ package com.android.dunderbaks.core.dashboard
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -20,10 +19,11 @@ class TapListFragment : Fragment() {
     @SuppressLint("AddJavascriptInterface", "SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
         val root = inflater.inflate(R.layout.fragment_tap_list, container, false)
         val webView = root.findViewById<WebView>(R.id.wvTapList)
+        webView.loadUrl("https://www.buildabeer.org/WhatsOnTapAt/WhatsOnTapAtApp.php?BarID=USAFL00014")
         webView.clearCache(true)
-        //        webView.getSettings().setDomStorageEnabled(true);
         webView.settings.javaScriptEnabled = true
         val jInterface = MyJavaScriptInterface(root.context)
         webView.addJavascriptInterface(jInterface, "HtmlViewer")
@@ -37,24 +37,23 @@ class TapListFragment : Fragment() {
                 view.evaluateJavascript("javascript:setTimeout(window.HtmlViewer.showHTML(document.getElementsByTagName('body')[0].innerHTML), 1000);", null)
 
 
-//                webView.loadUrl("javascript:window.HtmlViewer.showHTML(document.getElementsByTagName('script')[3].innerHTML)");
                 Thread {
+                    @Suppress("ControlFlowWithEmptyBody")
                     while (jInterface.html == null);
-                    //                     System.out.println(jInterface.html);
                     val doc = Jsoup.parse(jInterface.html)
                     val beerNames = doc.select("span.BName")
                     val beerInfo = doc.select("span.BI")
                     for (i in beerNames.indices) {
-                        println(beerNames[i].text())
-                        println(beerInfo[i].text())
+                        println("Beer Name: " + beerNames[i].text())
+                        println("Beer Info: " + beerInfo[i].text())
                     }
                 }.start()
             }
         }
-        webView.loadUrl("https://www.buildabeer.org/WhatsOnTapAt/WhatsOnTapAtApp.php?BarID=USAFL00014")
         return root
     }
 
+    @Suppress("unused")
     internal class MyJavaScriptInterface(private val ctx: Context) {
         var html: String? = null
 
@@ -62,6 +61,5 @@ class TapListFragment : Fragment() {
         fun showHTML(_html: String?) {
             html = _html
         }
-
     }
 }
